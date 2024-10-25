2d64ba21fd5d480b121849bd509aa20593c839ed
unknown <maryam>
2024-10-25 21:30:22 +0300

    write revert

--- lib/widget.dart
+++ lib/widget.dart
@@ -5,6 +5,7 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
+//!try to make rever..
